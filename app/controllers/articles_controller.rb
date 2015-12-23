225901f24985ef2d10ff8ec201d5bde0fd30ae0e
jen pengelly <jgpengelly@gmail.com>
2015-12-23 11:19:18 -0500

    full CRUD for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,57 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+# This shows me a list of all the articles
+  def index
+    @articles = Article.all
+  end
+
+# This shows me an individual Article
   def show
-    @article = Article.find(params[:id])
   end
 
 # Takes us to the template that renders a view of articles/new
   def new
   end
 
+# Updates an article
+  def edit
+  end
+
 # Saves the information from the form into the database
   def create
     @article = Article.new(article_params)
 
-    @article.save!
-    redirect_to @article
+    if @article.save!
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+# Applies the edits from the edit to controller to the database
+  def update
+
+    if @article.update!(article_params)
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+# Delete a single article from the database
+  def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
 # strong params
   private
     def article_params
       params.require(:article).permit(:title, :text)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
     end
   end
