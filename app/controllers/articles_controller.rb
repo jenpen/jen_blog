c8b0f913a3b5cb1e04531f96b81cda825e594b57
jen pengelly <jgpengelly@gmail.com>
2015-12-23 09:24:07 -0500

    initial set up for show route, controller, and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,24 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
 # Takes us to the template that renders a view of articles/new
   def new
   end
 
 # Saves the information from the form into the database
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save!
+    redirect_to @article
   end
 
-end
+# strong params
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+  end
